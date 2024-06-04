57f539da1b25444f0538a4a701fa5e207615c897
Yukun Zhang <yukun@yukuns-mbp.mynetworksettings.com>
2024-06-03 23:07:46 -0400

    mui - nav bar link tabs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
-
 const Home = () => {
-
-
   return (
+    <div className="Home_Wrapper">
       <div className="Home_Container">
         <h1 className="Home_Header">Welcome to the BookBuddy App!</h1>
         <h3 className="Home_Welcome">Happy Reading!!</h3>
       </div>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,81 @@
 /* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. 
 You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
+import {  useNavigate } from "react-router-dom";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Box, Tabs, Tab, Button } from "@mui/material";
 
-import { Link, useNavigate } from 'react-router-dom';
+const LinkTab = (props) => {
+  const { label, to, ...other } = props;
+  const navigate = useNavigate();
 
-const Navbar = ({isLoggedIn}) =>{
-   
-    const navigate = useNavigate();
+  const handleClick = (event) => {
+    event.preventDefault();
+    navigate(to);
+  };
 
-    const handleAccountOrLogin = () => {
-        if (isLoggedIn) {
-            navigate('/account')
-        } else {
-            navigate('/login')
-        }
+  return <Tab component="a" onClick={handleClick} label={label} {...other} />;
+};
 
-    }
+LinkTab.propTypes = {
+  label: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+};
 
-    return (
-        <div>
-            <button onClick={handleAccountOrLogin}>{isLoggedIn ? "Account" : "Login"} </button>
-            <ul>
-                <li>
-                    <Link to="./">Home</Link>
-                </li>
-                <li>
-                    <Link to="./books">Books</Link>
-                </li>
-            </ul>
-        </div>
-    )
+function samePageLinkNavigation(event) {
+  if (
+    event.defaultPrevented ||
+    event.button !== 0 ||
+    event.metaKey ||
+    event.ctrlKey ||
+    event.altKey ||
+    event.shiftKey
+  ) {
+    return false;
+  }
+  return true;
 }
+
+const Navbar = ({ isLoggedIn }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    // event.type can be equal to focus with selectionFollowsFocus.
+    if (
+      event.type !== "click" ||
+      (event.type === "click" && samePageLinkNavigation(event))
+    ) {
+      setValue(newValue);
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const handleAccountOrLogin = () => {
+    if (isLoggedIn) {
+      navigate("/account");
+    } else {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="nav tabs example"
+        role="navigation"
+        centered
+      >
+        <Button onClick={handleAccountOrLogin}>
+          {isLoggedIn ? "Account" : "Login"}{" "}
+        </Button>
+        <LinkTab label="Home" to="/" />
+        <LinkTab label="Books" to="/books" />
+      </Tabs>
+    </Box>
+  );
+};
 
 export default Navbar;
